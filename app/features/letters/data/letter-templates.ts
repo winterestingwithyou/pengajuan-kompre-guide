ea98e52cddd869653e806f4513b1447d37fce28f
Winterest | M. Adam Yudistira <153144749+winterestingwithyou@users.noreply.github.com>
2026-06-01 14:56:29 +0700

    Chore: Commenting all unused letter templates in letter-templates.ts

--- app/features/letters/data/letter-templates.ts
+++ app/features/letters/data/letter-templates.ts
@@ -27,20 +27,20 @@ export const letterTemplates: LetterTemplate[] = [
       "Generator surat validasi USEPT untuk kebutuhan kompre D3 Manajemen Informatika Unsri.",
     outputFileName: "validasi-usept.docx",
   },
-  {
-    id: "surat-pernyataan-bebas-plagiat",
-    title: "Surat Pernyataan Bebas Plagiat",
-    description:
-      "Generator surat pernyataan bebas plagiat yang dapat digunakan sebagai salah satu dokumen pengajuan kompre D3 Manajemen Informatika Unsri.",
-    outputFileName: "surat-pernyataan-bebas-plagiat.docx",
-  },
-  {
-    id: "surat-pernyataan-pemutakhiran-data",
-    title: "Surat Pernyataan Pemutakhiran Data",
-    description:
-      "Generator surat pernyataan telah melakukan pemutakhiran data untuk kebutuhan kompre D3 Manajemen Informatika Unsri.",
-    outputFileName: "surat-pernyataan-pemutakhiran-data.docx",
-  },
+// {
+//   id: "surat-pernyataan-bebas-plagiat",
+//   title: "Surat Pernyataan Bebas Plagiat",
+//   description:
+//     "Generator surat pernyataan bebas plagiat yang dapat digunakan sebagai salah satu dokumen pengajuan kompre D3 Manajemen Informatika Unsri.",
+//   outputFileName: "surat-pernyataan-bebas-plagiat.docx",
+// },
+// {
+//   id: "surat-pernyataan-pemutakhiran-data",
+//   title: "Surat Pernyataan Pemutakhiran Data",
+//   description:
+//     "Generator surat pernyataan telah melakukan pemutakhiran data untuk kebutuhan kompre D3 Manajemen Informatika Unsri.",
+//   outputFileName: "surat-pernyataan-pemutakhiran-data.docx",
+// },
 ];
 
 export function getLetterTemplate(templateId: string | undefined) {
