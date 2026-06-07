5d1c48857b7319cf230a828c33a3d482b716f42b
winterestingwithyour <yudistiraadam3@gmail.com>
2026-06-07 23:43:46 +0700

    feat: add google site verification meta tag in Layout component

--- app/root.tsx
+++ app/root.tsx
@@ -35,6 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="7fPd_w3RG_ieeGi2W_WFohcT33wFCZFDJAefoWYWUus" />
         <Meta />
         <Links />
       </head>
